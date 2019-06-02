e0e8804fad9f032afdb325f41f4871069bef293d
Legion <rom4ik730@gmail.com>
2019-06-02 21:18:24 +0300

    Simple Service for Toast displaying was added

--- app/src/main/java/com/legion1900/safedownload/SafeDownloaderService.java
+++ app/src/main/java/com/legion1900/safedownload/SafeDownloaderService.java
@@ -1,17 +1,50 @@
 package com.legion1900.safedownload;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
-import android.os.Binder;
+import android.os.Handler;
 import android.os.IBinder;
-import android.support.annotation.Nullable;
+import android.os.Message;
+import android.os.Messenger;
+import android.widget.Toast;
 
 public class SafeDownloaderService extends Service {
-    @Nullable
+
+    /*
+    * Message codes
+    * */
+    static final int MSG_HELLO_WORLD = 1;
+
+    /*
+    * Handler of incoming messages from clients
+    * */
+    private static class DownloadRequestHandler extends Handler {
+        private Context applicationContext;
+
+        DownloadRequestHandler(Context context) {
+            applicationContext = context.getApplicationContext();
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_HELLO_WORLD:
+                    Toast
+                            .makeText(applicationContext, "Hello World!", Toast.LENGTH_LONG)
+                            .show();
+                    break;
+                    default:
+                        super.handleMessage(msg);
+            }
+        }
+    }
+
+    private Messenger mMessenger;
+
     @Override
     public IBinder onBind(Intent intent) {
-        return new Binder() {
-
-        };
+        mMessenger = new Messenger(new DownloadRequestHandler(this));
+        return mMessenger.getBinder();
     }
 }
